1b152ed5b0fdd7f7c418c324e4dfd5c4786665bd
D. Azevedo <rplazevedo@gmail.com>
2023-11-19 15:04:42 +0000

    Added project files

---
+++ Assets/Script/Base.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    public float health;
+
+    private void Awake()
+    {
+        health = 3;
+    }
+
+    public void Damage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("A base died!");
+    }
+}
