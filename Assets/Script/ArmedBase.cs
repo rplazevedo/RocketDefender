14ae2cd5834a6d49badaa6b209e4513420b380e8
D. Azevedo <rplazevedo@gmail.com>
2023-11-20 18:29:34 +0000

    Limited fire rate

--- Assets/Script/ArmedBase.cs
+++ Assets/Script/ArmedBase.cs
@@ -8,10 +8,17 @@ public class ArmedBase : Base
     private Rocket rocket;
     [SerializeField]
     private float rocketSpeed = 3f;
+
+    [SerializeField]
+    private float fireRate;
+    private float shootTime;
+
+
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && CanShoot())
         {
             Fire();
         }
@@ -19,9 +26,16 @@ public class ArmedBase : Base
 
     void Fire()
     {
+        shootTime = Time.time;
         Rocket rocketInstance = Instantiate(rocket);
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction = mousePos - transform.position;
         rocketInstance.Launch(direction, rocketSpeed);
+    }
+
+
+    public bool CanShoot()
+    {
+        return  Time.time >= shootTime + fireRate;
     }
 }
